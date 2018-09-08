ca8f7506ef95c55ac23f148b4a0eb255256ac485
GCoder10 <https://github.com/GCoder10/Web-api.git>
2018-09-08 13:23:44 +0200

    Prepare messages component, set up route into messages in navbar.
    1. Use data from resolver ( when component is starting ), which name is messages, as we set up it in the routing.
    2. Method for load new messages, also when we change the current page.

--- src/app/workers/components/messages/messages.component.ts
+++ src/app/workers/components/messages/messages.component.ts
@@ -1,3 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from './../../../auth/shared/services/auth.service';
+import { UserService } from './../../../shared/services/user.service';
+import { Pagination, PaginatedResult } from './../../../shared/models/Pagination';
+import { Message } from './../../../shared/models/Message';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,12 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  BgMessages: string;
+  messages: Message[];
+  pagination: Pagination;
+  messageContainer = 'Unread';
 
-
-  constructor() { }
+  constructor(private userService: UserService,
+              private authService: AuthService,
+              private route: ActivatedRoute ) { }
 
   ngOnInit() {
-
+    this.BgMessages = 'assets/images/BGsignin2.jpg';
+    this.route.data.subscribe(data => {
+      this.messages = data['messages'].result;
+      this.pagination = data['messages'].pagination;
+    });
   }
+
+  loadMessages() {
+    var alertify = require('alertifyjs/build/alertify.js');
+    // tslint:disable-next-line:max-line-length
+    this.userService.getMessages(this.authService.decodedToken.nameid, this.pagination.currentPage, this.pagination.itemsPerPage, this.messageContainer)
+                    .subscribe((res: PaginatedResult<Message[]>) => {
+                      this.messages = res.result;
+                      this.pagination = res.pagination;
+                    }, error => {
+                      alertify.error(error);
+                    });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadMessages();
+  }
+
 
 }
